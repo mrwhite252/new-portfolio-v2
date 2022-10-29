0f7ec1d67a47e7e4947201794042c657f9e16b75
David Bai <baiminghan@gmail.com>
2022-10-29 22:01:07 +1300

    add the main section

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head';
+import Main from '../components/Main';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
-  return (<div>
-    <Head>
-      <title> David Portfolio </title>
-      <meta name="description" content="David Bai's protfolio site" />
-      <link rel="icon" href="/favicon.svg" />
-    </Head>
+  return (
 
-    <Navbar />
-  </div>
+    <div>
+      <Head>
+        <title> David Portfolio </title>
+        <meta name="description" content="David Bai's protfolio site" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Navbar />
+      <Main />
+    </div>
   )
 }
