a91a9e50b21a2b94b1c9df9600aaa7163aa0cf4a
David Bai <baiminghan@gmail.com>
2022-10-29 21:34:14 +1300

    add menu open function working

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import navLogo from "../public/assets/logo-img.svg";
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -31,7 +39,7 @@ const Navbar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
-                    <div className='md:hidden'>
+                    <div className='md:hidden' onClick={handleNav}>
 
                         <AiOutlineMenu size={25} />
 
@@ -41,13 +49,17 @@ const Navbar = () => {
             </div>
 
 
-            <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-                <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+            <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                <div className={
+                    nav
+                        ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'
+                        : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                }>
                     <div>
                         <div className='flex w-full items-center justify-between'>
                             <Image src={navLogo} alt='/' width='50' height='50' />
                             <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
-                                <AiOutlineClose />
+                                <AiOutlineClose onClick={handleNav} />
                             </div>
                         </div>
 
